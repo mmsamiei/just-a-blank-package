57ea17b2f0fd9ac6486ef69e4aeb6d469daf9d41
Mahdi Samiei <mahdi.samiei@sotoon.ir>
2024-02-05 19:00:31 +0330

    change name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your-package-name',
+    name='justablank',
     version='0.0.0',
     packages=find_packages(),
     install_requires=[]
