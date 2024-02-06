1d03b7f18c76c7db36d1f86adffe3f1082a8b540
Mahdi Samiei <mahdi.samiei@sotoon.ir>
2024-02-06 13:40:11 +0330

    add init

--- justablank/module1.py
+++ justablank/module1.py
@@ -2,3 +2,15 @@ import numpy as np
 
 def return_hello(name):
     return 'hello ' + name + '!'
+
+def random_tensor(shape):
+    """
+    Generate a random numpy tensor with given shape.
+
+    Parameters:
+    shape (tuple): The shape of the tensor.
+
+    Returns:
+    numpy.ndarray: A random tensor with the specified shape.
+    """
+    return np.random.random(shape)
