f60c05d83aa3a9a2f534c3c7d8c87eb5c2967acb
Mahdi Samiei <mahdi.samiei@sotoon.ir>
2024-02-06 14:07:32 +0330

    asdasdasdasdsadasdasd

--- justablank/__init__.py
+++ justablank/__init__.py
@@ -1,7 +1,7 @@
 # from .module1 import *
 # from .module2 import *
-# __all__ = [
-#     "module1",
-#     "module2"
-# ]
-from . import *
+__all__ = [
+    "module1",
+    "module2"
+]
+# from . import *
